bb921397e019818b01d05cf6f184345a86de3188
Eng Haitham Farahat Eldeeb <haisamfarahat@outlook.sa>
2025-04-08 12:03:43 +0200

    Add missing files

--- Program.cs
+++ Program.cs
@@ -1,5 +1,16 @@
+using HospitalAPP.Genrics.WorkGenrics;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NewsApi.AppHandler.ErrorHandler;
+using NewsApi.AppHandler.Genrics.Intrefaces;
+using NewsApi.Infastructure.AppInfa;
+using NewsApi.Infastructure.IdentityInfa;
+using NewsApi.Infastructure.AppInfa.Migrations;
+using NewsAPI.Domain.IdentityEntity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
-namespace NewsApi
+namespace NewsAPIApp
 {
     public class Program
     {
@@ -8,7 +19,34 @@ namespace NewsApi
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddDbContext<AppDbContext>(
+                opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+                );
+            builder.Services.AddDbContext<IdentityDbContext>(
+               opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("AppIdentityConnection"))
+               );
 
+            builder.Services.AddIdentity<Account, IdentityRole>()
+                            .AddEntityFrameworkStores<IdentityDbContext>();
+
+            builder.Services.AddScoped(typeof(IUnitOfWork), typeof(UnitOfWork));
+
+            builder.Services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.InvalidModelStateResponseFactory = (actionContext) =>
+                {
+                    var errors = actionContext.ModelState.Where(p => p.Value.Errors.Count() > 0)
+                                                         .SelectMany(p => p.Value.Errors)
+                                                         .Select(e => e.ErrorMessage)
+                                                         .ToArray();
+                    var ValidationErrorResponse = new ApiValidationErrorResponse()
+                    {
+                        Errors = errors
+                    };
+
+                    return new BadRequestObjectResult(ValidationErrorResponse);
+                };
+            });
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
@@ -25,8 +63,8 @@ namespace NewsApi
 
             app.UseHttpsRedirection();
 
+            app.UseAuthentication();
             app.UseAuthorization();
-
 
             app.MapControllers();
 
